60586720c8f685dc919162cc2999cdba7c3a2430
ruanwillians <ruanwillians.mn@gmail.com>
2023-02-18 13:33:02 -0300

    alteração modo route

--- quasar.config.js
+++ quasar.config.js
@@ -47,7 +47,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: 'history', // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
@@ -68,9 +68,9 @@ module.exports = configure(function (ctx) {
 
       // https://v2.quasar.dev/quasar-cli-webpack/handling-webpack
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-      
+
       chainWebpack (/* chain */) {}
-      
+
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-devServer
@@ -89,10 +89,10 @@ module.exports = configure(function (ctx) {
           primary: '#000',
           secondary: '#26A69A',
           accent: '#9C27B0',
-  
+
           dark: '#1d1d1d',
           'dark-page': '#121212',
-  
+
           positive: '#21BA45',
           negative: '#C10015',
           info: '#31CCEC',
@@ -134,9 +134,9 @@ module.exports = configure(function (ctx) {
       maxAge: 1000 * 60 * 60 * 24 * 30,
         // Tell browser when a file from the server should expire from cache (in ms)
 
-      
+
       chainWebpackWebserver (/* chain */) {},
-      
+
 
       middlewares: [
         ctx.prod ? 'compression' : '',
@@ -151,9 +151,9 @@ module.exports = configure(function (ctx) {
 
       // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
       // if using workbox in InjectManifest mode
-      
+
       chainWebpackCustomSW (/* chain */) {},
-      
+
 
       manifest: {
         name: `Quasar App`,
@@ -227,13 +227,13 @@ module.exports = configure(function (ctx) {
       },
 
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-      
-      chainWebpackMain (/* chain */) {},
-      
 
-      
+      chainWebpackMain (/* chain */) {},
+
+
+
       chainWebpackPreload (/* chain */) {},
-      
+
     }
   }
 });
